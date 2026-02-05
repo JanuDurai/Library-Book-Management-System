ae1a42b6ad97125ee02ec5e263e7df84e7e2e83d
Janu Nagarajan <janu.nagarajan@aspiresys.com>
2026-02-05 11:59:03 +0530

    user auth and interface implemented

--- Api/bookApi.php
+++ Api/bookApi.php
@@ -1,26 +1,81 @@
 <?php
 header("Content-Type: application/json");
 
-require_once "../Auth/UserAuth.php";
-require_once "../Model/Book.php";
+spl_autoload_register(function ($className) {
+    $baseDir = dirname(__DIR__);
 
-$auth = new UserAuthenticate();
+    $folders = ['Auth', 'Model'];
+
+    foreach ($folders as $folder) {
+        $file = $baseDir . "/$folder/$className.php";
+        if (file_exists($file)) {
+            require_once $file;
+            return;
+        }
+    }
+});
+
+$auth = new UserAuth();
 $auth->authenticate();
 
-$title = $_POST['title'] ?? null;
-$isbn = $_POST['isbn'] ?? null;
-$format = $_POST['format'] ?? null;
+$data = $_POST;
 
-if (!$title || !$isbn || !$format) {
-    echo json_encode(["error" => "Missing book data"]);
-    exit;
+$errors = isValidRequest($data);
+
+if (! empty($errors)) {
+    http_response_code(400);
+    echo json_encode($errors);
+    exit();
+} else {
+    if (isUserRequest($data)) {
+        $user = new User($data);
+        echo json_encode($user->getDetails());
+        exit();
+    } else if (isBookRequest($data)) {
+        $book = new Book($data);
+        echo json_encode($book->getDetails());
+        exit();
+    }
 }
 
-if (!in_array($format, ["Physical", "Digital"])) {
-    echo json_encode(["error" => "Invalid format"]);
-    exit;
+function isUserRequest(array $data)
+{
+    return isset($data['name'], $data['memberId'], $data['email']);
 }
 
-$book = new Book($title, $isbn, $format);
+function isBookRequest(array $data)
+{
+    return isset($data['title'], $data['isbn'], $data['format'],
+        $data['author'], $data['year_of_publish']);
+}
 
-echo json_encode($book->getDetails());
+function isValidRequest(array $data)
+{
+    $errors = [];
+    if (isUserRequest($data)) {
+        $attributes = ["name", "memberId", "email"];
+    } else if (isBookRequest($data)) {
+        $attributes = ["title", "isbn", "format", "author", "year_of_publish"];
+    }
+
+    foreach ($attributes as $attr) {
+        if (empty($data[$attr])) {
+            $errors[$attr] = ucwords(str_replace("_", " ", $attr)) . ' is required';
+            return $errors;
+        }
+    }
+
+    if (count($attributes) === 3) {
+        if (! filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+            $errors['email'] = 'Invalid email format';
+        }
+    }
+
+    if (count($attributes) === 5) {
+        if (! in_array($data['format'], ['physical', 'digital', "Physical", "Digital"])) {
+            $errors['format'] = "Invalid book format";
+        }
+    }
+
+    return $errors;
+}
